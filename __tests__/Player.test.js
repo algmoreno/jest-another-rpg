0c6b45064b5f71857737950eb7c3856ae69c1375
algmoreno <alg.moreno00@gmail.com>
2021-06-27 23:05:09 -0700

    mock tests

---
+++ __tests__/Player.test.js
@@ -0,0 +1,18 @@
+const Potion = require('../lib/Potion');
+const Player = require('../lib/Player');
+
+console.log(new Potion());
+
+jest.mock('../lib/Potion'); 
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number)); 
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    )
+})
